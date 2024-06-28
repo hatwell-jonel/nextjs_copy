a0d347b1e8e6061af823ca41306e5e38869c78b7
Jonel Hatwell <jonel.hatwell@gmail.com>
2024-06-28 17:41:00 +0800

    can connect to db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,38 @@
+// LOCAL
+
+// import { drizzle } from "drizzle-orm/mysql2";
+// import mysql from "mysql2/promise";
+
+// const poolConnection = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   database: "temp_next_crud",
+// });
+
+// export const db = drizzle(poolConnection);
+
+
+
+// FOR SERVERLESS
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
+import * as schema from "./schema";
+import { env } from "../../env";
 
-const poolConnection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: "temp_next_crud",
+
+const globalPool: mysql.Pool | undefined = undefined;
+
+function connect() {
+  if (typeof globalPool !== "undefined") {
+    return globalPool;
+  }
+  return mysql.createPool({
+    uri: env.DB_URL,
+  });
+}
+
+export const db = drizzle(connect(), {
+  schema,
+  mode: "default",
+  // logger: process.env.NODE_ENV === "development",
 });
-
-export const db = drizzle(poolConnection);
